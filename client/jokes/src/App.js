65452eaf3fd262a03b468d9527ba4517b09be706
Thierry Joux <tjoux@hotmail.com>
2019-06-14 09:56:00 -0700

    Setup App.js

--- client/jokes/src/App.js
+++ client/jokes/src/App.js
@@ -1,14 +1,33 @@
 import React from 'react';
+import Signup from './components/Signup';
+import Signin from './components/Signin';
+import Jokes from './components/Jokes';
+import {NavLink, withRouter, Route} from 'react-router-dom';
+
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        Employee Web Site
+        <NavLink to='/signin'>Login</NavLink>
+        <NavLink to='/signup'>Sign Up</NavLink>
+        <button onClick={logout}>Logout</button>
       </header>
+
+      <Route path="/signin" component={Signin} />
+      <Route path='/signup' component={Signup} />
+      <Route path='/users' component={Jokes} />
     </div>
   );
 }
 
-export default App;
+const logout = () => {
+  localStorage.removeItem('token');
+  this.props.history.push('/signin');
+};
+
+
+export default withRouter(App);
